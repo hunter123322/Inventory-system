1fe5bddd4300c8748b9746e7644bc705ad81e763
hunter123322 <drin123322@gmail.com>
2024-06-26 03:21:21 -0700

    Add index.html

---
+++ model/databaseConnection.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+//Your actual URL for mongoDB
+const url = require("./databaseURL..js");
+
+const connectToDatabase = async () => {
+  mongoose.connect(url);
+
+  //This line of code will catch the error if it's connection failed
+  mongoose.connection.on("error", (err) => {
+    console.error(`Connection error at ${err}`);
+  });
+
+  // This code will apear to connection seccessfull
+  mongoose.connection.once("open", () => {
+    console.log("MongoDB connection opened!");
+  });
+};
+
+module.exports = connectToDatabase;
